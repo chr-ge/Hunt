f2e9f61c5cade2f28de947548585a144db4443ae
chr-ge <ace26@protonmail.com>
2020-10-12 16:54:33 -0400

    Firebase Setup

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDzBCTJEpnggTDt1jLj9rbLrgEpmvFapyM",
+  authDomain: "hunt-dc9df.firebaseapp.com",
+  databaseURL: "https://hunt-dc9df.firebaseio.com",
+  projectId: "hunt-dc9df",
+  storageBucket: "hunt-dc9df.appspot.com",
+  messagingSenderId: "1008885854682",
+  appId: "1:1008885854682:web:c0a861f88f7b6b62e490e3",
+};
---
+++ src/firebase/firebase.ts
@@ -0,0 +1,21 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import { firebaseConfig } from './config';
+
+class Firebase {
+  app: typeof app;
+  auth: app.auth.Auth;
+  db: app.firestore.Firestore;
+  
+  constructor() {
+    app.initializeApp(firebaseConfig);
+    this.app = app;
+    this.auth = app.auth();
+    this.db = app.firestore();
+  }
+}
+
+const firebase = new Firebase();
+export default firebase;
