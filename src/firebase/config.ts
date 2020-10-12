620cc8190cfe0ce166ede2355186fc5c3b322914
George <61952924+chr-ge@users.noreply.github.com>
2020-10-12 21:32:41 +0000

    Delete config.ts

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBrXBvr2tXvqBnMV_g83mvDK8egigNyApM",
-  authDomain: "hunt-b0a05.firebaseapp.com",
-  databaseURL: "https://hunt-b0a05.firebaseio.com",
-  projectId: "hunt-b0a05",
-  storageBucket: "hunt-b0a05.appspot.com",
-  messagingSenderId: "16645664200",
-  appId: "1:16645664200:web:5b141e2f24d1b6012b24fd"
-};
