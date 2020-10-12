ed29a9c1a4d3c003d17937259d6186a33bcb9918
chr-ge <ace26@protonmail.com>
2020-10-12 17:06:28 -0400

    Firebase Auth

---
+++ src/firebase/config.example.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "<your_apiKey>",
+  authDomain: "<your_authDomain>",
+  databaseURL: "<your_databaseURL>",
+  projectId: "<your_projectId>",
+  storageBucket: "<your_storageBucket>",
+  messagingSenderId: "<your_messagingSenderId>",
+  appId: "<your_appId>",
+};
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -15,6 +15,28 @@ class Firebase {
     this.auth = app.auth();
     this.db = app.firestore();
   }
+
+  async register (name: string, email: string, password: string) {
+    const newUser = await this.auth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
+    return newUser.user?.updateProfile({
+      displayName: name
+    })
+  }
+
+  login (email: string, password: string) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.auth.signOut();
+  }
+
+  resetPassword(email: string) {
+    return this.auth.sendPasswordResetEmail(email);
+  }
 }
 
 const firebase = new Firebase();
