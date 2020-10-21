bd5899b8bc0f65bfbdc49e3720e3e354965e1492
chr-ge <ace26@protonmail.com>
2020-10-20 21:24:09 -0400

    Added ProductList

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { RouteComponentProps } from 'react-router';
 import { IonContent, IonPage } from "@ionic/react";
+import ProductList from "../components/Product/ProductList";
 import SmallHeader from "../components/Header/SmallHeader";
 import LargeHeader from "../components/Header/LargeHeader";
 
-const Home = () => {
+const Home = ({ location }: RouteComponentProps) => {
   return (
     <IonPage>
       <SmallHeader title="Hunt" />
-      <IonContent fullscreen>
+      <IonContent color="medium" fullscreen>
         <LargeHeader title="Hunt" />
+        <br></br>
+        <ProductList />
       </IonContent>
     </IonPage>
   );
--- src/pages/Trending.tsx
+++ src/pages/Trending.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { RouteComponentProps } from 'react-router';
 import { IonContent, IonPage } from "@ionic/react";
 import SmallHeader from "../components/Header/SmallHeader";
 import LargeHeader from "../components/Header/LargeHeader";
+import ProductList from "../components/Product/ProductList";
 
-const Trending = () => {
+const Trending = ({ location }: RouteComponentProps) => {
   return (
     <IonPage>
       <SmallHeader title="Trending" />
-      <IonContent fullscreen>
-        <LargeHeader title="Trending" />
+      <IonContent color="medium" fullscreen>
+        <LargeHeader title="Hunt" />
+        <br></br>
+        <ProductList />
       </IonContent>
     </IonPage>
   );
